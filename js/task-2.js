38f69b45c646c16eb07c6d296e23909ecbae18d1
saregokcan <gokcansare@gmail.com>
2025-06-22 15:45:15 +0300

    .

---
+++ js/task-2.js
@@ -0,0 +1,7 @@
+function makeArray(firstArray, secondArray, maxLength) {
+  const newArray = firstArray.concat(secondArray);
+  if (newArray.length > maxLength) {
+    newArray.length = maxLength;
+  }
+  return newArray;
+}
---
+++ js/task-3.js
@@ -0,0 +1,11 @@
+function filterArray(numbers, value) {
+  const newArray = [];
+
+  for (const number of numbers) {
+    if (number > value) {
+      newArray.push(number);
+    }
+  }
+
+  return newArray;
+}
